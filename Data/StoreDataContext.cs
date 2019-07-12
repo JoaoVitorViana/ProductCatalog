96ff8e14474c5c7920ecd6c5b318b335a5d3211a
JoaoVitorViana <joaovitor@live.it>
2019-07-12 16:44:11 -0400

    update migrations

--- Data/StoreDataContext.cs
+++ Data/StoreDataContext.cs
@@ -1,22 +1,23 @@
-using Microsoft.EntityDrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data.Maps;
 using ProductCatalog.Models;
 
 namespace ProductCatalog.Data
 {
-    public class StoreDataContext : DbContext 
-    {
-        public DbSet<Product> Products { get; set; }
-        public Dbset<Category> Categorys { get; set; }
+	public class StoreDataContext : DbContext
+	{
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categorys { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=localhost,1444;Database=pdtctl;User ID=SA;Password=1q2w3e%&!");
-        }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=localhost\sqlexpress;Database=pdtctl;User ID=SA;Password=1q2w3e%&!");
+		}
 
-        // protected override void OnModelCreating(ModelBuilder builder)
-        // {
-        //     builder.ApplyConfiguration(new ProductMap());
-        //     builder.ApplyConfiguration(new CategoryMap());
-        // }
-    }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.ApplyConfiguration(new ProductMap());
+			builder.ApplyConfiguration(new CategoryMap());
+		}
+	}
 }
